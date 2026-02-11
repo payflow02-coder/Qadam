813f912a50255838fd05290562a9210d2be07ad4
payflow02-coder <payflow02@gmail.com>
2026-02-12 01:41:27 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
